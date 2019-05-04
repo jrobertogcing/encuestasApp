0a2a18012d01b81be04c6f6ccf38e31625c20f3d
José González <josegonzalez@MacBook-Pro-de-INFRA.local>
2019-05-04 13:18:07 -0500

    Se agrega template para home.

--- encuestas/encuestas/settings.py
+++ encuestas/encuestas/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'encuestas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['encuestas/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
