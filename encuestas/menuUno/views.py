ee4d3d4ed0085596d5abbe0a347cc009aad211b5
José González <josegonzalez@MacBook-Pro-de-INFRA.local>
2019-05-04 12:49:14 -0500

    Se agrega vista para menuUno

--- encuestas/menuUno/views.py
+++ encuestas/menuUno/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from.models import Encuesta
+
+def encusta_lista(request):
+    encuestas = Encuesta.objects.all()
+    return HttpResponse(encuestas)
