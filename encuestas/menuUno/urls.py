5b65e32342b71aa6b46dfd41992a42db0f66fc06
José González <josegonzalez@MacBook-Pro-de-INFRA.local>
2019-05-09 13:25:42 -0500

    Se crea detallde de persona html

--- encuestas/menuUno/urls.py
+++ encuestas/menuUno/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.encusta_lista),
+    path('<int:encuesta_pk>/<int:persona_pk>', views.persona_detail),
     path('<int:pk>/', views.encuesta_detail),
 ]
--- encuestas/menuUno/views.py
+++ encuestas/menuUno/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from.models import Encuesta
+from.models import Encuesta, Persona
 
 def encusta_lista(request):
     encuestas = Encuesta.objects.all()
@@ -12,3 +12,8 @@ def encuesta_detail(request, pk):
     # encuesta = Encuesta.objects.get(pk=pk)
     encuesta = get_object_or_404(Encuesta, pk=pk)
     return render(request, 'encuestas/detalle_encuesta.html', {'encuesta': encuesta})
+
+def persona_detail(request, encuesta_pk, persona_pk):
+    persona = get_object_or_404(Persona, encuesta_id=encuesta_pk, pk=persona_pk)
+    return render(request, 'encuestas/persona_detail.html',{'persona': persona})
+
