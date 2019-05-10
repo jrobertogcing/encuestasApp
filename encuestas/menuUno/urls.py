013ca92dda7773216991bc4c97452785c6731552
José González <josegonzalez@MacBook-Pro-de-INFRA.local>
2019-05-10 13:06:00 -0500

    Se agregan links para navegar en enucestas y encuestados

--- encuestas/menuUno/urls.py
+++ encuestas/menuUno/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 
 from . import views
 
+app_name = 'menuUno'
 urlpatterns = [
     path('', views.encusta_lista, name='encuesta_list'),
-    path('<int:encuesta_pk>/<int:persona_pk>', views.persona_detail),
-    path('<int:pk>/', views.encuesta_detail),
+    path('<int:encuesta_pk>/<int:persona_pk>', views.persona_detail, name='persona_detail'),
+    path('<int:pk>/', views.encuesta_detail, name='encuesta_detail'),
 ]
