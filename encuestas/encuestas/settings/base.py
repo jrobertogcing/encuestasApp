1a6f035c5b1e918d60c1052c76674cca985bdfa4
Dragon <dragon@MacBook-de-Dragon.local>
2019-07-09 21:30:34 -0700

    se cambia host

--- encuestas/encuestas/settings/base.py
+++ encuestas/encuestas/settings/base.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 # ALLOWED_HOSTS = ['192.168.100.205', 'localhost', '127.0.0.1' ]
 # ALLOWED_HOSTS = ['18.223.136.99' , 'localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ec2-18-223-136-99.us-east-2.compute.amazonaws.com', '18.223.136.99']
 
 
 # Application definition
--- encuestas/encuestas/settings/production.py
+++ encuestas/encuestas/settings/production.py
@@ -1,4 +1,4 @@
 from .base import *
 DEBUG = False
 # ALLOWED_HOSTS = ['18.223.136.99' , 'localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ec2-18-223-136-99.us-east-2.compute.amazonaws.com', '18.223.136.99']
