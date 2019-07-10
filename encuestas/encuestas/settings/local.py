b45666e55ecb09bd79d43b06d8a02c3c16ea8e21
Dragon <dragon@MacBook-de-Dragon.local>
2019-07-09 21:11:23 -0700

    Se cambian parametros de configuración host

--- encuestas/encuestas/settings/local.py
+++ encuestas/encuestas/settings/local.py
@@ -1,4 +1,5 @@
 from .base import *
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.223.136.99' , 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['18.223.136.99' , 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
--- encuestas/encuestas/settings/production.py
+++ encuestas/encuestas/settings/production.py
@@ -1,4 +1,4 @@
 from .base import *
-DEBUG = 
-False
-ALLOWED_HOSTS = ['18.223.136.99' , 'localhost', '127.0.0.1']
+DEBUG = False
+# ALLOWED_HOSTS = ['18.223.136.99' , 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
